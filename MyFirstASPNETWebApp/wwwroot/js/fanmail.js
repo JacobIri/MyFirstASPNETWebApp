e6f77666d32284527f63276c280194bcc536d90d
JacobIri <76026530+JacobIri@users.noreply.github.com>
2022-01-04 21:44:48 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,15 @@
+﻿    //document.getElementById("buttonsend").addEventListener("click", function() {
+    //alert("From: " + document.getElementById("textFrom").value +
+    //    "\n" + "Subject: " + document.getElementById("textSubject").value +
+    //    "\n" + "Message: " + document.getElementById("textMessage").value
+    //);
+    //})
+
+
+    $("#buttonsend").click(function () {
+        alert("From: " + $("#textFrom").val() +
+            "\n" + "Subject: " + $("#textSubject").val() +
+            "\n" + "Message: " + $("#textMessage").val()
+        );
+        $("#gandersonPhoto").fadeOut("slow");
+    })
